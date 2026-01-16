67fa4966ada0b9056490e296620230f766547280
huwu <hugogonzalez3063@gmail.com>
2026-01-16 16:25:59 -0300

    :bug: fix: datos de 'infeccion' corregidos y leyenda de modelo SIR corregida

--- webmap/map/models.py
+++ webmap/map/models.py
@@ -41,7 +41,7 @@ class Traffic(models.Model):
     tiempo_entre_largo = models.FloatField()
     DCA = models.FloatField()
     nivel_congestion = models.IntegerField(null=True, blank=True)
-    infeccion = models.IntegerField()
+    infeccion = models.IntegerField(null=True, blank=True)
 
 
 
